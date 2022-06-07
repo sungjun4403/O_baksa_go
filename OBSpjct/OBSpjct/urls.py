8cab654ff51aa4d0c26486077096f43104611819
sungjun4403 <geulligu89@naver.com>
2022-06-07 22:27:40 +0900

    .

--- OBSpjct/OBSpjct/urls.py
+++ OBSpjct/OBSpjct/urls.py
@@ -3,6 +3,8 @@ from django.urls import path
 from OBSapp import views as obs
 from django.conf.urls.static import static
 from django.conf import settings
+from django.views.static import serve
+from django.urls import re_path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -11,5 +13,7 @@ urlpatterns = [
     path('get_options/', obs.get_options, name = "get_options"),
     path('end/', obs.end, name = "end"),
     path('get_map/', obs.get_map, name = "get_map"),
-
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    #DEBUG = False 일떄도 collect static 하게 해주는 부분
+    re_path(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+]
