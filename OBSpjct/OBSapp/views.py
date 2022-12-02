db99fcf651043f143ae989b863234c17fb58f332
성준(Sungjun) <96364048+sungjun4403@users.noreply.github.com>
2022-12-03 01:21:38 +0900

    Update views.py

--- OBSpjct/OBSapp/views.py
+++ OBSpjct/OBSapp/views.py
@@ -30,6 +30,11 @@ import heapq
 matplotlib.use('Agg')
 ox.settings.use_cache = True
 # ox.settings.log_console=True
+''' IDK WHT TO DO
+IDK WHT TO DO
+IDK WHT TO DOIDK WHT TO DO
+IDK WHT TO DO
+'''
 ox.__version__
 
 def home (request):
